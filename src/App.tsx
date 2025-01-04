76e955dd120d2a4e96d70afeffd48965bbdc7f7a
Dilawar Khan <130894444+Dilawarkhaninfo@users.noreply.github.com>
2025-01-04 14:28:04 -0500

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Skills from './pages/Skills';
-import Contact from './pages/Contact';
 import Footer from './components/Footer';
 
 const App: React.FC = () => {
@@ -13,9 +10,6 @@ const App: React.FC = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
 
